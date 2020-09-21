89823d52c939025cfbe21637f5473702caee9a92
Charles Liu <dodoinblue@gmail.com>
2020-09-22 00:43:23 +0800

    Controller test

---
+++ src/auth/auth.controller.spec.ts
@@ -0,0 +1,41 @@
+import { Test } from '@nestjs/testing';
+import { DatabaseModule } from '../database/database.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+// jest.mock('./auth.service');
+
+describe('AuthController', () => {
+  let authService: AuthService;
+  let authController: AuthController;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [DatabaseModule],
+      controllers: [AuthController],
+      providers: [AuthService],
+    }).compile();
+    authService = moduleRef.get<AuthService>(AuthService);
+    authController = moduleRef.get<AuthController>(AuthController);
+  });
+
+  describe('login', () => {
+    it('should return an array of cats', async () => {
+      const result = {
+        accessToken: 'token',
+        refreshToken: 'rToken',
+        id: 'id',
+        username: 'name',
+      };
+      const myMock = jest.fn(async () => result);
+      jest
+        .spyOn(authService, 'loginWithUsernamePassword')
+        .mockImplementation(myMock);
+
+      expect(
+        await authController.login({ username: 'name', password: 'pwd' }),
+      ).toBe(result);
+
+      expect(myMock.mock.calls[0]).toEqual(['name', 'pwd']);
+    });
+  });
+});
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,7 +3,6 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { LoginDto } from './dtos/login.dto';
 import { LoginResponse, RegistrationResponse } from './auth.responses';
 import { RegisterDto } from './dtos/register.dto';
-import { AuthModel } from '../database/models/auth.model';
 import { AuthService } from './auth.service';
 import { RefreshDto } from './dtos/refresh.dto';
 
